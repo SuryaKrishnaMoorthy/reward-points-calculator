b1efec7c4445fb648a97cd52f13105b3015594e0
Surya Krishna Moorthy <suryakrishnamoorthy@Suryas-MacBook-Pro.local>
2022-11-21 09:11:44 -0500

    add header test

---
+++ src/components/header/__tests__/Header.test.js
@@ -0,0 +1,10 @@
+import {render, screen} from "@testing-library/react";
+import Header from "../Header";
+
+describe("Header", () => {
+    it("should render the heading as 'Rewards Calculator'", () => {
+        render(<Header />);
+        const heading = screen.getByText(/Rewards Calculator/i);
+        expect(heading).toBeInTheDocument();
+    })
+})
